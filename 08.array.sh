a96f672887f8024381973d11b524c67a71f49c03
mohan <mohanprasad390@gmail.com>
2025-05-26 04:36:07 +0530

    datatypes started

---
+++ 08.array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MOVIES=("Court" "HIT 3" "Pushpa 1" "Pushpa 2")
+
+echo "First Movie : ${MOVIES[0]}"
+
+echo "Second Movie : ${MOVIES[1]}"
+
+echo "Third Movie : ${MOVIES[2]}"
+
+echo "Fourth Movie : ${MOVIES[3]}"
+
+echo "All Movies : ${MOVIES[@]}"
