5b471ceb7ae0e6fb9b1c9a2ae29fc32759681693
mohan <mohanprasad390@gmail.com>
2025-05-26 04:09:48 +0530

    variable started

---
+++ 06.variable.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "What is your Name ::"
+
+read NAME
+
+echo "What is your age ::"
+
+read AGE
+
+echo "Where are you living?"
+
+read LIVING
+
+echo "your name is :: $NAME"
+
+echo "your age is :: $AGE"
+
+echo "you are living in :: $LIVING"
