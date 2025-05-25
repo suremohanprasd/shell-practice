70d2de1992ceb446fd1b3cfcb1f96f8c8a94eb3e
mohan <mohanprasad390@gmail.com>
2025-05-26 03:32:49 +0530

    Covo started

--- 02.convo.sh
+++ 02.convo.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Hi Iam Mohan Prsad Sure"
+echo "Mohan  :: Hi Prasad"
+echo "Prasad :: Hi Mohan, How are you?"
+echo "Mohan  :: Iam fine Mohan, What about you"
+echo "Prasad :: Iam fine too, what are the plans for weekend?"
+echo "Mohan  :: Iam palnning to visit the temple with friends"
+echo "Prasad :: Nice, carry on."
