21ca039f6a758040d29bdec0eff755fd474b7d01
mohan <mohanprasad390@gmail.com>
2025-05-26 03:55:10 +0530

    variable started

---
+++ 04.variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1  :: Hi $PERSON2"
+echo "$PERSON2 :: Hi $PERSON1, How are you?"
+echo "$PERSON1  :: Iam fine $PERSON1, What about you"
+echo "$PERSON2 :: Iam fine too, what are the plans for weekend?"
+echo "$PERSON1  :: Iam palnning to visit the temple with friends"
+echo "$PERSON2 :: Nice, carry on."
