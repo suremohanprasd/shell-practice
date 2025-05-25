427dca12bac22e3319784e4aa53f067032ded64b
mohan <mohanprasad390@gmail.com>
2025-05-26 04:25:29 +0530

    datatypes started

--- 07.data-types.sh
+++ 07.data-types.sh
@@ -4,7 +4,12 @@ TIMESTAMP=$(date)
 
 echo "Script executed at : $TIMESTAMP"
 
-echo Enter first Number = (read N1)
-echo Enter Second Number =(read N2)
+echo "Enter first Number :"
+
+read N1
+
+echo "Enter Second Number :" 
+
+read N2
 
 echo "Sum of first and second number is : $N1+$N2"
