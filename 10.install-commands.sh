a3992c7efa7b4ed19855d24368be1cbec325f474
mohan <mohanprasad390@gmail.com>
2025-05-27 05:03:47 +0530

    conditions started

--- 10.install-commands.sh
+++ 10.install-commands.sh
@@ -2,29 +2,30 @@
 
 USERID=$(id -u)
 
-# if [ $USERID -ne 0 ]
-# then
-#     echo "ERROR:: Please run the script with root access"
-# else
-#     echo "Changing to root user"
-# sudo su -
-# fi
-
-if [ $USERID -eq 0 ]
+if [ $USERID -ne 0 ]
 then
+    echo "ERROR:: Please run the script with root access"
+else
     echo "You are running with root access"
-    exit 1
-else
-        echo "changing to root user"
 fi
-sudo su -
 
-dnf install mysql -y
 
-if [ $? -eq 0 ]
+dnf list installed mysql
+
+if [ $? -ne 0 ]
 then
-    echo "Installing MySQL is Succcess"
-else
-    echo "Installing MySQL is Failed"
+    echo "MySQL is not insalled... going to install it."
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySQL is Succcess"
+    else
+        echo "Installing MySQL is Failed"
     exit 1
 fi
+else
+    echo "MySQL is already installed.. Nothing to do"
+    exit 1
+fi
+
+
