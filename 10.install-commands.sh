c11063f5cdfe08a22e80b07e3c6bc61e479cea65
mohan <mohanprasad390@gmail.com>
2025-05-27 04:12:09 +0530

    conditions started

--- 10.install-commands.sh
+++ 10.install-commands.sh
@@ -2,10 +2,19 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+# if [ $USERID -ne 0 ]
+# then
+#     echo "ERROR:: Please run the script with root access"
+# else
+#     echo "Changing to root user"
+# sudo su -
+# fi
+
+if [ $USERID -eq 0 ]
 then
-    echo "ERROR:: Please run the script with root access"
-else
-    echo "Changing to root user"
-sudo su -
+    echo "You are running with root access"
+    exit 1
+    if [ $USERID -ne 0 ]
+    then "changing to root user"
+    sudo su -
 fi
