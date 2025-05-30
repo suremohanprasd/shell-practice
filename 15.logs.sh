c437349709cce092a4685a4257d38d33820ffdaf
mohan <mohanprasad390@gmail.com>
2025-05-31 04:05:10 +0530

    logs.sh

--- 15.logs.sh
+++ 15.logs.sh
@@ -5,7 +5,7 @@ G="-e \e[32m"
 Y="-e \e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/logs/shellscript-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." f1)    # $0 -> have the script name
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
