4cabf50d32b80208e07c9bc743fd1f177314c3a5
mohan <mohanprasad390@gmail.com>
2025-05-31 02:34:35 +0530

    colors

--- 13.colors.sh
+++ 13.colors.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo -e "\e[33m Mohan Prasad \e[0m"
+# echo -e "\e[33m Mohan Prasad \e[0m"
 
-echo "Hello No colors"
+# echo "Hello No colors"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e $R "Hi, Iam Mohan Prasad" $N
+
+echo -e $G "I am Learning Devops with AWS" $N
+
+echo -e $N "I am living in Hyderabad"
