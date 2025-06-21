900f2b4b94573bf3a98913bb986fed38e26a475f
mohan <mohanprasad390@gmail.com>
2025-06-21 13:51:43 +0530

    message

--- 15.logs.sh
+++ 15.logs.sh
@@ -6,10 +6,11 @@ Y="-e \e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." f1)    # $0 -> have the script name
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)    # $0 -> have the script name
+mkdir -p $LOGS_FOLDER
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOLDER
+
 echo "Script started executing at : $(date)" &>>$LOG_FILE
 
 
