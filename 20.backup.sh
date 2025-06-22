053da02cce6c655dffb911c2bf44192301fa3c72
mohan <mohanprasad390@gmail.com>
2025-06-22 11:28:40 +0530

    message

--- 20.backup.sh
+++ 20.backup.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
+USERID=$(id -u)
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14}
+DAYS=${3:-14} # if DAYS are provided that will be considered, otherwise default 14 days
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N"
+        exit 1 #give other than 0 upto 127
+    else
+        echo "You are running with root access" 
+    fi
+}
+
+check_root
+mkdir -p $LOGS_FOLDER
 
 USAGE(){
-    echo "USAGE :: sh.usage.sh <source-directory> <destination-directory> <days(optional)>"
+    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
+    exit 1
 }
 
 if [ $# -lt 2 ]
@@ -15,39 +49,39 @@ fi
 
 if [ ! -d $SOURCE_DIR ]
 then
-    echo "Source Directory $SOURCE_DIR does not exit. Please check"
+    echo -e "$R Source Directory $SOURCE_DIR does not exist. Please check $N"
     exit 1
 fi
 
 if [ ! -d $DEST_DIR ]
 then
-    echo "Destination Directory $DEST_DIR does not exit. Please check"
+    echo -e "$R Destination Directory $DEST_DIR does not exist. Please check $N"
     exit 1
 fi
 
-FILES=$(find SOURCE_DIR -name "*.log" -mtime +$DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ ! -z $FILES ]
+if [ ! -z "$FILES" ]
 then
-    echo "Files to zip are : $FILES"
+    echo "Files to zip are: $FILES"
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    echo $FILES | zip -@ $ZIP_FILE
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 
     if [ -f $ZIP_FILE ]
     then
-        echo "Succesfully created zip file"
+        echo -e "Successfully created Zip file"
+
         while IFS= read -r filepath
         do
-          echo "Deleting file : $filepath"
-          rm -rf $filepath
+            echo "Deleting file: $filepath" | tee -a $LOG_FILE
+            rm -rf $filepath
         done <<< $FILES
-
-        echo "Log files older than $DAYS from source directory removed.... SUCCESFULLY"
+        echo -e "Log files older than $DAYS from source directory removed ... $G SUCCESS $N"
     else
-        echo "Zip file creation... FAILURE"
+        echo -e "Zip file creation ... $R FAILURE $N"
+        exit 1
     fi
 else
-    echo "No log files older than 14 days... SKIPPING"
+    echo -e "No log files found older than 14 days ... $Y SKIPPING $N"
 fi
-
